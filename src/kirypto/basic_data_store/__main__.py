af22195f55117d0e116f4689d79da1d97fdbff26
kirypto <kirypto@gmail.com>
2022-12-24 17:36:21 -0600

    Create a __main__ script in basic_data_store package to run the application

---
+++ src/kirypto/basic_data_store/__main__.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from typing import Dict, Any
+from argparse import ArgumentParser
+
+from ruamel.yaml import YAML
+
+from kirypto.basic_data_store.application.app_main import BasicDataStoreApp
+
+
+def _main() -> None:
+    kwargs = _parse_args()
+    app_args = kwargs.pop("config")
+    try:
+        app = BasicDataStoreApp(**app_args)
+    except TypeError as e:
+        raise TypeError(f"Failed to construct app, invalid configuration: {e}")
+    app.run()
+
+
+def _parse_args() -> Dict[str, Any]:
+    parser = ArgumentParser(description="Basic Data Store Application")
+    parser.add_argument("config", type=yaml_config, help="Config for the application.")
+    args = parser.parse_args()
+    return {
+        key: val
+        for key, val in vars(args).items()
+        if val is not None
+    }
+
+
+def yaml_config(config_file: str) -> dict:
+    config: dict = YAML(typ="safe").load(Path(config_file))
+    return config
+
+
+if __name__ == "__main__":
+    _main()

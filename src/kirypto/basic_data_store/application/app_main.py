102edf77494e49741fcb49edfeb3afed1750c99b
kirypto <kirypto@gmail.com>
2022-12-26 16:39:24 -0600

    Created an Item domain object

--- src/kirypto/basic_data_store/application/app_main.py
+++ src/kirypto/basic_data_store/application/app_main.py
@@ -1,4 +1,7 @@
+from json import dumps, loads
 from logging import warning
+
+from kirypto.basic_data_store.domain.objects import Item
 
 
 class BasicDataStoreApp:
@@ -7,6 +10,23 @@ class BasicDataStoreApp:
             warning(f"Received unwanted keyword arguments: {{{', '.join(kwargs.keys())}}}; ignoring.")
 
         print(database_config)
+        print(f"Constructing valid {Item.__name__}...")
+        item = Item(id="abad1dea-0000-0000-0000-000000000000", value="Value")
+        print(f"Done: {item}")
+        print(f"Constructing INVALID {Item.__name__}...")
+        try:
+            invalid = Item(id="abad1dea-0000-0000-0000-000000000000")
+            print(f"SHOULD NOT SEE THIS! Somehow constructed: {invalid}")
+        except TypeError as e:
+            print(f"Failed as expected. Error: {e}")
+        print("As JSON:")
+        item_as_json = dumps(item)
+        print(item_as_json)
+        print("From JSON:")
+        print(loads(item_as_json))
+        print("From JSON (as Item):")
+        print(Item(loads(item_as_json)))
+
 
     def run(self) -> None:
         pass
---
+++ src/kirypto/basic_data_store/domain/objects.py
@@ -0,0 +1,32 @@
+from typing import Dict, Any
+from uuid import UUID
+
+
+def _validate_domain_object(domain_object: Any, *, expected_member_types_by_name: Dict[str, type]) -> None:
+    for property_name, expected_type in expected_member_types_by_name.items():
+        try:
+            if not hasattr(domain_object, property_name):
+                raise TypeError(f"Missing argument '{property_name}' for {domain_object.__class__}")
+            actual = getattr(domain_object, property_name)
+            if not isinstance(actual, expected_type):
+                raise ValueError(f"Argument '{property_name}' must be {expected_type}, was {type(actual)}")
+        except KeyError:
+            raise TypeError(f"Missing argument '{property_name}' for {domain_object.__class__}")
+
+
+class Item(dict):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        _validate_domain_object(self, expected_member_types_by_name=dict(
+            id=UUID,
+            value=str,
+
+        ))
+
+    @property
+    def id(self) -> UUID:
+        return UUID(self["id"])
+
+    @property
+    def value(self) -> str:
+        return self["value"]

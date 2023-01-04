beb9c17bba5e28f93e6fcd93bcfb6125a49b3d47
kirypto <kirypto@gmail.com>
2023-01-03 22:13:19 -0600

    Tie in auth facade to remaining routes

--- src/kirypto/basic_data_store/application/routes.py
+++ src/kirypto/basic_data_store/application/routes.py
@@ -19,30 +19,34 @@ def register_item_routes(rest_server: RestServer, item_facade: ItemFacade, auth_
         return HTTPStatus.CREATED, dumps(item)
 
     @rest_server.register_rest_endpoint("/api/items", "get", auth_token=True)
-    def get_items(_auth_token: str) -> HandlerResult:
-        info(f"GET /api/items invoked")
+    @auth_facade.translate_auth_param
+    def get_items(auth_token_name: AuthTokenName) -> HandlerResult:
+        info(f"GET /api/items invoked with token '{auth_token_name}'")
         ids = item_facade.get_item_ids()
         info(f"Returning {len(ids)} ids")
         return HTTPStatus.OK, dumps([str(id) for id in ids])
 
     @rest_server.register_rest_endpoint("/api/item/<item_id>", "get", auth_token=True)
-    def get_item_id(_auth_token: str, *, item_id: str) -> HandlerResult:
-        info(f"GET /api/item/<item_id> invoked with id '{item_id}'")
+    @auth_facade.translate_auth_param
+    def get_item_id(auth_token_name: AuthTokenName, *, item_id: str) -> HandlerResult:
+        info(f"GET /api/item/<item_id> invoked with id '{item_id}' and token '{auth_token_name}'")
         item = item_facade.get_item(UUID(item_id))
         info(f"Returning item: {item.id}")
         return HTTPStatus.OK, dumps(item)
 
     @rest_server.register_rest_endpoint("/api/item/<item_id>", "put", json=True, auth_token=True)
-    def get_item_id(body: Any, _auth_token: str, *, item_id: str) -> HandlerResult:
-        info(f"PUT /api/item/<item_id> invoked with id '{item_id}'")
+    @auth_facade.translate_auth_param
+    def get_item_id(body: Any, auth_token_name: AuthTokenName, *, item_id: str) -> HandlerResult:
+        info(f"PUT /api/item/<item_id> invoked with id '{item_id}' and token '{auth_token_name}'")
         item = Item(id=item_id, value=body)
         item_facade.update_item(item)
         info(f"Updated item: {item.id}")
         return HTTPStatus.OK, dumps(item)
 
     @rest_server.register_rest_endpoint("/api/item/<item_id>", "delete", auth_token=True)
-    def delete_item_id(_auth_token: str, *, item_id: str) -> HandlerResult:
-        info(f"DELETE /api/item/<item_id> invoked with id '{item_id}'")
+    @auth_facade.translate_auth_param
+    def delete_item_id(auth_token_name: AuthTokenName, *, item_id: str) -> HandlerResult:
+        info(f"DELETE /api/item/<item_id> invoked with id '{item_id}' and token '{auth_token_name}'")
         item_facade.delete(UUID(item_id))
         info(f"Removed item: {item_id}")
         return HTTPStatus.NO_CONTENT, ""
